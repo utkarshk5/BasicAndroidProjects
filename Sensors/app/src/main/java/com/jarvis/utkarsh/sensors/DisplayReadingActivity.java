f4de5a7ca192afdc648b964f60c58dba88e615f1
Utkarsh Kumar <kumar.utkarsh007@gmail.com>
2016-05-09 10:48:20 +0530

    Added an app that dumps the output from various sensors

---
+++ Sensors/app/src/main/java/com/jarvis/utkarsh/sensors/DisplayReadingActivity.java
@@ -0,0 +1,13 @@
+package com.jarvis.utkarsh.sensors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayReadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_reading);
+    }
+}
